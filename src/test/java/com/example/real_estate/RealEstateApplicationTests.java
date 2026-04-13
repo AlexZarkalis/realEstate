e77c16f9f724f13d381e0908e172b7f6218c9809
AlexZarkalis <zarkalisalexandros@gmail.com>
2026-04-13 15:37:17 +0300

    added simple auth tests for jenkins pipeline

--- src/test/java/com/example/real_estate/RealEstateApplicationTests.java
+++ src/test/java/com/example/real_estate/RealEstateApplicationTests.java
@@ -1,13 +1,45 @@
 package com.example.real_estate;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class RealEstateApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testCreateUser() throws Exception {
+		mockMvc.perform(post("/saveUser")
+						.param("username", "testuser123")
+						.param("email", "testuser@example.com")
+						.param("password", "password123")
+						.with(csrf()))
+				.andExpect(status().isOk())
+				.andExpect(view().name("index"))
+				.andExpect(model().attributeExists("msg"))
+				.andExpect(model().attribute("msg", containsString("saved successfully")));
+	}
+
+	@Test
+	public void testSignUser() throws Exception {
+		mockMvc.perform(get("/login"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("auth/login"));
 	}
 
 }
